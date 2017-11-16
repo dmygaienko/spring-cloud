f95aea3275480d78779328d846f6076f717e2f79
dmygaien <dmygaenko@gmail.com>
2017-11-16 22:26:04 +0200

    api

--- spring-cloud-zuul/src/main/java/com/mygaienko/ApiProxy.java
+++ spring-cloud-zuul/src/main/java/com/mygaienko/ApiProxy.java
@@ -3,6 +3,7 @@ package com.mygaienko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
 
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
-@Import({SecurityConfig.class, SessionConfig.class})
+@EnableDiscoveryClient
+@Import({SecurityConfig.class/*, SessionConfig.class*/})
 public class ApiProxy {
 
     public static void main(String[] args) {
